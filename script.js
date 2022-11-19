78e416cf1e794990a41445deda4d3edb21ec6fad
Facundo <facuvilla1998@gmail.com>
2022-11-18 21:10:21 -0300

    signo = modificado

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ btn.addEventListener('click',btnSubmit);
 function btnSubmit(){
     // event.preventDefault(); se usa para detener el evento predeterminado del boton submit del formulario que hacia enviar lo datos del form a la URL por lo que la pagina se recargaba y mostraba el resultado solo por una milesima de segundo
     const result = +input1.value +  +input2.value;
-    h2Result.innerText= 'Resultado= ' + result;
+    h2Result.innerText = 'Resultado= ' + result;
 }
 
 //con la funcion y evento de abajo podemos mostrar una alerta con un mensaje que dice que el texto fue copiado
